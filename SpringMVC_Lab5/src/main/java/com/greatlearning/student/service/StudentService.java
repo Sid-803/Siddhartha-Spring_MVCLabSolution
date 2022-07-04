86fe9effbef55a5a32b070fb37478d87bd8f6cfa
Siddhartha <siddharthchauhan888@gmail.com>
2022-07-04 17:45:50 +0530

    Created SessionFactory Object and Autowired it to inject with sessionFactory bean in servlet.xml

--- SpringMVC_Lab5/src/main/java/com/greatlearning/student/service/StudentService.java
+++ SpringMVC_Lab5/src/main/java/com/greatlearning/student/service/StudentService.java
@@ -2,12 +2,17 @@ package com.greatlearning.student.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.SessionFactoryUtils;
 import org.springframework.stereotype.Service;
 
 import com.greatlearning.student.entity.Student;
 
 @Service
 public class StudentService {
+	
+	@Autowired
+	private SessionFactoryUtils sessionFactory;
 	
 	public List<Student> findStudents(){
 		
